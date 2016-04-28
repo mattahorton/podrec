e51f9a7b3aa239f08dc257b3edbc358a9aef4b01
mattahorton <matthew.a.horton@gmail.com>
2016-04-27 18:33:15 -0700

    Personalize demo

--- script.js
+++ script.js
@@ -10,17 +10,19 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi! I\'m Smooch Bot!')
+            return bot.say('Hey y\'all! I built this bot because some of you have been ' +
+             'interested in me providing podcast recommendations. Also, I wanted to ' + 
+             'build a bot.')
                 .then(() => 'askName');
         }
     },
 
     askName: {
-        prompt: (bot) => bot.say('What\'s your name?'),
+        prompt: (bot) => bot.say('Just humor me here and type your name so this thing can know what to call you. Feel free to enter something like \'Queen Bey\' or \'Sunshine\' or really whatever you want.'),
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name}`))
+                .then(() => bot.say(`Great. That's setlled. What can I do for you, ${name}?`))
                 .then(() => 'finish');
         }
     },
@@ -28,8 +30,9 @@ module.exports = new Script({
     finish: {
         receive: (bot, message) => {
             return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
-                        'teach me how to do anything else!'))
+                .then((name) => bot.say(`Sorry ${name}, this bot is pretty ` +
+                        'dumb, so you should message me (as in Matt) and ' + 
+                        'tell me to make it smarter.'))
                 .then(() => 'finish');
         }
     }
